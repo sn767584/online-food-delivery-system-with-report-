29e9675cdb184450fba125e4ce91ab20b9b9cb93
sn767584 <52776375+sn767584@users.noreply.github.com>
2020-01-03 11:48:11 +0530

    Add files via upload

---
+++ Online food delivery/sql/food.sql
@@ -0,0 +1,409 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 03, 2020 at 06:31 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `food`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `price` int(11) NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `price`, `deleted`) VALUES
+(30, 'Chicken Biryani', 80, 0),
+(31, 'Chocolate Cake', 40, 0),
+(32, 'Chicken Noodles', 60, 0),
+(33, 'Chicken Burger', 45, 0),
+(34, 'Veg Fired Rice', 50, 0),
+(35, 'Veg Biryani', 45, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `payment_type` varchar(16) NOT NULL DEFAULT 'Wallet',
+  `total` int(11) NOT NULL,
+  `status` varchar(25) NOT NULL DEFAULT 'Yet to be delivered',
+  `deleted` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_id`, `address`, `description`, `date`, `payment_type`, `total`, `status`, `deleted`) VALUES
+(18, 7, 'a-41,11 palace road, mysore.', '', '2019-11-26 18:34:39', 'Cash On Delivery', 535, 'Delivered', 0),
+(19, 7, 'a-41,11 palace road, mysore.', '', '2019-12-09 17:06:10', 'Cash On Delivery', 320, 'Yet to be delivered', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `order_id`, `item_id`, `quantity`, `price`) VALUES
+(35, 18, 30, 1, 80),
+(36, 18, 33, 2, 90),
+(37, 18, 31, 3, 120),
+(38, 18, 35, 1, 45),
+(39, 18, 34, 4, 200),
+(40, 19, 30, 1, 80),
+(41, 19, 32, 1, 60),
+(42, 19, 35, 4, 180);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL,
+  `poster_id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `description` varchar(3000) NOT NULL,
+  `status` varchar(8) NOT NULL DEFAULT 'Open',
+  `type` varchar(30) NOT NULL DEFAULT 'Others',
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `poster_id`, `subject`, `description`, `status`, `type`, `date`, `deleted`) VALUES
+(1, 2, 'Subject 1', 'New Description for Subject 1', 'Answered', 'Support', '2017-03-30 18:08:51', 0),
+(2, 6, 'Delivery Issues', 'Food not yet delivered', 'Closed', 'Complaint', '2019-11-01 16:32:53', 0),
+(3, 3, 'food problem', 'food was expired so pls go through it', 'Answered', 'Complaint', '2019-11-02 13:20:22', 0),
+(4, 7, 'Delivery speed', 'make it quicker as much as possible.\r\nthank u.', 'Answered', 'Complaint', '2019-12-09 17:05:30', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket_details`
+--
+
+CREATE TABLE `ticket_details` (
+  `id` int(11) NOT NULL,
+  `ticket_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ticket_details`
+--
+
+INSERT INTO `ticket_details` (`id`, `ticket_id`, `user_id`, `description`, `date`) VALUES
+(1, 1, 2, 'New Description for Subject 1', '2017-03-30 18:08:51'),
+(2, 1, 2, 'Reply-1 for Subject 1', '2017-03-30 19:59:09'),
+(3, 1, 1, 'Reply-2 for Subject 1 from Administrator.', '2017-03-30 20:35:39'),
+(4, 1, 1, 'Reply-3 for Subject 1 from Administrator.', '2017-03-30 20:49:35'),
+(5, 2, 6, 'Food not yet delivered', '2019-11-01 16:32:53'),
+(6, 2, 1, 'We are working on that', '2019-11-01 16:34:40'),
+(7, 3, 3, 'food was expired so pls go through it', '2019-11-02 13:20:22'),
+(8, 3, 1, 'sorry sir we will make sure that this issue will be considered seriously', '2019-11-02 13:44:48'),
+(9, 4, 7, 'make it quicker as much as possible.\r\nthank u.', '2019-12-09 17:05:30'),
+(10, 4, 1, 'thank you for your complaint\r\nwe are working on it, in future we will provide a better facility to delivery a food as fast as possible. ', '2019-12-09 17:13:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `role` varchar(15) NOT NULL DEFAULT 'Customer',
+  `name` varchar(15) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `email` varchar(35) DEFAULT NULL,
+  `address` varchar(300) DEFAULT NULL,
+  `contact` bigint(11) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT 0,
+  `deleted` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role`, `name`, `username`, `password`, `email`, `address`, `contact`, `verified`, `deleted`) VALUES
+(1, 'Administrator', 'Admin ', 'root', 'toor', 'admin99@gmail.com', 'a&d food shop,mangalore', 9898000000, 1, 0),
+(2, 'Customer', 'Akash', 'user1', 'pass1', 'akash@example.com', 'a-80,manipal.', 9898000001, 1, 0),
+(3, 'Customer', 'Rakesh', 'user2', 'pass2', 'rakesh@example.com', 'b-90,shivamoga.', 9898000002, 1, 0),
+(4, 'Customer', 'Rajesh', 'user3', 'pass3', 'raju@gmail.com', 'h3,mangalore', 9898000003, 0, 0),
+(5, 'Customer', 'Rahul', 'user4', 'pass4', 'rahul@gmail.com', 'h-18s,bangalore.', 9898000004, 0, 0),
+(6, 'Customer', 'Salim', 'user5', 'pass5', 'salim@gmail.com', 'u-42w,udupi.', 7654321456, 0, 0),
+(7, 'Customer', 'Geetha', 'user6', 'pass6', 'geetha@gmail.com', 'a-41,11 palace road, mysore.', 7541254312, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet`
+--
+
+CREATE TABLE `wallet` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `wallet`
+--
+
+INSERT INTO `wallet` (`id`, `customer_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet_details`
+--
+
+CREATE TABLE `wallet_details` (
+  `id` int(11) NOT NULL,
+  `wallet_id` int(11) NOT NULL,
+  `number` varchar(16) NOT NULL,
+  `cvv` int(3) NOT NULL,
+  `balance` int(11) NOT NULL DEFAULT 2000
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `wallet_details`
+--
+
+INSERT INTO `wallet_details` (`id`, `wallet_id`, `number`, `cvv`, `balance`) VALUES
+(1, 1, '6155247490533921', 983, 3430),
+(2, 2, '1887587142382050', 772, 1850),
+(3, 3, '4595809639046830', 532, 1455),
+(4, 4, '5475856443351234', 521, 2000),
+(5, 5, '1234567890123456', 123, 1410),
+(6, 6, '7894561237894561', 545, 2000),
+(7, 7, '7633614102038891', 75, 2000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `poster_id` (`poster_id`);
+
+--
+-- Indexes for table `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ticket_id` (`ticket_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `customer_id` (`customer_id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `wallet_details`
+--
+ALTER TABLE `wallet_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `wallet_id` (`wallet_id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `wallet`
+--
+ALTER TABLE `wallet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `wallet_details`
+--
+ALTER TABLE `wallet_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`poster_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  ADD CONSTRAINT `ticket_details_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`),
+  ADD CONSTRAINT `ticket_details_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD CONSTRAINT `wallet_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `wallet_details`
+--
+ALTER TABLE `wallet_details`
+  ADD CONSTRAINT `wallet_details_ibfk_1` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
